a0d2155f25db848769850a72ccdd8098dc681c09
uy <a.normurzaev20@gmail.com>
2026-04-15 22:39:26 +0500

    main.js ichida function yozilmoqda

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const wrapper = document.getElementById("container");
 
 
-fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
+fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
 .then (response => response.json())
 .then (data => {
+    renderData(data)
 
 })
 
 
-function renderData() {
+function renderData(data) {
     const cards = data.map(el => `
-        <div>
-           <img src="" alt="">
-           <h1></h1>
+        <div key = {el.id}>
+           <img src=${el.avatar} alt="">
+           <h1>${el.name}</h1>
    </div>
-        `)
+        `).join("")
+
+        wrapper.innerHTML = cards
 }
