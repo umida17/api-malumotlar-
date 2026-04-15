464656903b1f8f7250089cd547f1f7e6c15696bf
uy <a.normurzaev20@gmail.com>
2026-04-15 22:42:23 +0500

    input

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const wrapper = document.getElementById("container");
+let searchInput = document.getElementById("input");
 
 
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
@@ -19,3 +20,14 @@ function renderData(data) {
 
         wrapper.innerHTML = cards
 }
+
+
+
+searchInput.addEventListener("input", function(e) {
+   let malumot = e.target.value.toLowerCase();
+  const filterData = datas.filter(user => 
+    user.title.toLowerCase().includes(malumot)
+  );
+  renderUser(filterData)
+})
+renderUser(datas)
