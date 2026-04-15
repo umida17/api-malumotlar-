8590493043763408ec528781f6690d58618faa3e
uy <a.normurzaev20@gmail.com>
2026-04-15 22:32:21 +0500

    main.js yozilmoqda

---
+++ main.js
@@ -0,0 +1,13 @@
+const wrapper = document.getElementById("container");
+
+
+fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Students")
+.then (response => response.json())
+.then (data => {
+
+})
+
+
+function renderData() {
+    const 
+}
