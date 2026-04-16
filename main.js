687ae99eb2d9ab6dba836523bd31426d7d0de0eb
uy <a.normurzaev20@gmail.com>
2026-04-16 20:30:30 +0500

    css da stil berilvati

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 const wrapper = document.getElementById("container");
 let searchInput = document.getElementById("input");
 
+let datas = []; // 🔥 global saqlash
 
 fetch("https://699d9b4283e60a406a46e1ba.mockapi.io/Teachers")
-.then (response => response.json())
-.then (data => {
-    renderData(data)
-
-})
-
+.then(response => response.json())
+.then(data => {
+    datas = data;
+    renderData(datas);
+});
 
 function renderData(data) {
     const cards = data.map(el => `
-        <div id= "malumot" key = {el.id}>
-           <img id= "rasm" src=${el.avatar} alt="">
+        <div id="malumot">
+           <img id="rasm" src="${el.avatar}" alt="">
            <h1>${el.name}</h1>
-   </div>
-        `).join("")
+        </div>
+    `).join("");
 
-        wrapper.innerHTML = cards
+    wrapper.innerHTML = cards;
 }
 
-
-
 searchInput.addEventListener("input", function(e) {
-   let malumot = e.target.value.toLowerCase();
-  const filterData = datas.filter(user => 
-    user.title.toLowerCase().includes(malumot)
-  );
-  renderUser(filterData)
-})
-  
+    let malumot = e.target.value.toLowerCase();
+
+    const filterData = datas.filter(user => 
+        user.name.toLowerCase().includes(malumot)
+    );
+
+    renderData(filterData); 
+});
